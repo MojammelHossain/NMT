6af900b4fa6067d0634670d54892ba4fc025098c
MojammelHossain <mojammel.hossain@northsouth.edu>
2020-07-23 11:31:31 +0600

    package setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='nmt_models',
+      version='0.1',
+      description='Neural Mechine Translation',
+      packages=['nmt_models'],
+      author = 'Mojammel Hossain',
+      author_email = 'asifmojammel@gmail.com',
+    zip_safe=False)
